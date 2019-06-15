0afb3ccff7289bc1d2940c33ae3552836363f94a
Bear <robinca@hotmail.com>
2019-06-15 14:37:17 -0400

    Add full data files

--- analysis.py
+++ analysis.py
@@ -7,4 +7,44 @@ import pandas as pd
 
 data = pd.read_csv('data/eos.csv', delimiter = ',')
 df = pd.DataFrame(data)
-print(df)
+#print(df)
+
+for col in data.columns: 
+    print(col)
+
+
+
+# find the columns for "date" and "price(USD)"
+
+
+
+
+
+# calculate interday return (difference between price of 2 consecutive days)
+
+
+
+
+
+# calculate volatility 30d (standard deviation of the interday return over the past 30 days)
+
+
+
+
+
+# calculate the ROI 30d (difference between price of any day and price 30 days ago)
+
+
+
+
+# sort the table based on volatility (small to big)
+
+
+
+
+# select rows with volatility <=0.2
+    #calculate correlation (linear regression: what is the slope?) of volatility and ROI
+
+
+# select rows with volatility >0.2
+    #calculate correlation (linear regression: what is the slope?) of volatility and ROI
